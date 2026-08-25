6d64b02cce6227df7f0536937125b6db57e1b8b4
Your Name <you@example.com>
2026-08-25 00:42:27 -0400

    fix: tipo de dato de edad

--- src/presenter.js
+++ src/presenter.js
@@ -12,7 +12,7 @@ form.addEventListener("submit", (event) => {
 
   const nombre = nombreinput.value;
   const genero = generoSelect.value;
-  const edad = edadinput.value;
+  const edad = Number.parseInt(edadInput.value); 
   const idioma = idiomaSelect.value;
 
   div.innerHTML = "<p>" + saludar(nombre,genero,edad,idioma) + "</p>";
